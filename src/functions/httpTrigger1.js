6ec0a34c3848b0d673b549257ba5583259387a9b
Ben Medcalf <ben.medcalf@gmail.com>
2024-08-16 22:41:27 -0400

    add function http trigger

---
+++ src/functions/httpTrigger1.js
@@ -0,0 +1,13 @@
+const { app } = require('@azure/functions');
+
+app.http('httpTrigger1', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for request "${JSON.stringify(request)}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+
+        return { body: `Hello, ${name}!` };
+    }
+});
